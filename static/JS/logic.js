e4cd44db0764c8fa9b39e0105f083e4d478d7f94
Shannon Young <Shay@MacBook-Pro.local>
2025-02-27 20:32:11 -0600

    finished visuals

--- static/JS/logic.js
+++ static/JS/logic.js
@@ -15,11 +15,11 @@ let baseMap1 = L.tileLayer(
 ).addTo(myMap1);
 
 // open object for rent layers
-let rentLayers = {};
+var rentLayers = {};
 //set the years
-let years = [2020, 2021, 2022, 2023, 2024];
-//define rent color scale 
-let rentColorScale=[]
+var years = [2020, 2021, 2022, 2023, 2024];
+//define rent color scale
+var rentColorScale = ["#78f76d", "#135218"];
 
 //define GeoJSON data
 d3.json("output/updated.geojson").then(function (response) {
@@ -27,26 +27,32 @@ d3.json("output/updated.geojson").then(function (response) {
   //loop through each year and create a choropleth layer for each one
   years.forEach(function (year) {
     rentLayers[`Rent ${year}`] = L.choropleth(response, {
- //function to determine which property of the geojson to use for fill color
+      //function to determine which property of the geojson to use for fill color
       valueProperty: function (feature) {
-      
+        if (feature.properties.rent && feature.properties.rent[year]) {
+          return feature.properties.rent[year];
+        } else {
+          return null;
+        }
       },
-    // set the color scale for the choropleth
+      // set the color scale for the choropleth
       scale: rentColorScale,
-      steps: ,
+      steps: 10,
       mode: "q",
       style: function (feature) {
-      //determine value of rentValue and handling if there is not one
-      //use if/else statement 
-        let rentValue 
-        
-
+        //determine value of rentValue and handling if there is not one
+        //use if/else statement
+        let rentValue;
+        if (feature.properties.rent) {
+          rentValue = feature.properties.rent[year];
+        } else {
+          rentValue = null;
+        }
         if (rentValue === null) {
           return {
-            color: "gray",
+            color: "white",
             weight: 1,
-            fillOpacity: 1,
-            fillColor: "gray",
+            fillOpacity: 0,
           };
         } else {
           return {
@@ -56,41 +62,52 @@ d3.json("output/updated.geojson").then(function (response) {
           };
         }
       },
-      //function to bind pop up and handle state with no data 
+      //function to bind pop up and handle state with no data
       onEachFeature: function (feature, layer) {
-        let rentText =
-        layer.bindPopup();
+        let rentText;
+        if (feature.properties.rent) {
+          rentText = feature.properties.rent[year];
+        } else {
+          rentText = `No data`;
+        }
+        layer.bindPopup(`<b>${feature.properties.name}:</b> ${rentText}`);
       },
     });
   });
   L.control.layers(null, rentLayers, { collapsed: false }).addTo(myMap1);
   // have rent 2020 layer open upon loading
   rentLayers[`Rent 2020`].addTo(myMap1);
-  // create the legend 
-  let legend = L.control({position:""})
-    // Add the legend to the map
-  legend.onAdd = function () {
+
+  // create the legend
+  let rentLegend = L.control({ position: "bottomleft" });
+  // Add the legend to the map
+  rentLegend.onAdd = function () {
     let div = L.DomUtil.create("div", "info legend");
-    let limits = 
-    let colors = 
+    let limits = [750, 925, 1100, 1275, 1450, 1625, 1800, 1975, 2150, 2325];
+    let colors = rentLayers[`Rent 2020`].options.colors;
     let labels = [];
 
     // Add min & max values to the legend
-    div.innerHTML = '<div class="labels"><div class="min">' + limits[0] + '</div> \
-        <div class="max">' + limits[limits.length - 1] + '</div></div>';
+    div.innerHTML =
+      '<div class="labels"><div class="min">' +
+      limits[0] +
+      '</div> \
+          <div class="max">' +
+      limits[limits.length - 1] +
+      "</div></div>";
 
     // Loop through the limits and add color boxes for each range
     limits.forEach(function (limit, index) {
       labels.push('<li style="background-color: ' + colors[index] + '"></li>');
     });
 
-    div.innerHTML += '<ul>' + labels.join("") + "</ul>";
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
     return div;
   };
 
   // Add the legend to the map
-  legend.addTo(myMap1);
-})
+  rentLegend.addTo(myMap1);
+});
 
 // initialize map 2 (mortgage)
 // Create the map object with center on Denver and set zoom.
@@ -107,6 +124,101 @@ let baseMap2 = L.tileLayer(
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }
 ).addTo(myMap2);
+
+// open object for rent layers
+let mortgageLayers = {};
+
+//define rent color scale
+let mortgageColorScale = ["#3ecff7", "#0d4757"];
+
+//define GeoJSON data
+d3.json("output/updated.geojson").then(function (response) {
+  console.log(response);
+  //loop through each year and create a choropleth layer for each one
+  years.forEach(function (year) {
+    mortgageLayers[`Mortgage ${year}`] = L.choropleth(response, {
+      //function to determine which property of the geojson to use for fill color
+      valueProperty: function (feature) {
+        if (feature.properties.mortgage && feature.properties.mortgage[year]) {
+          return feature.properties.mortgage[year];
+        } else {
+          return null;
+        }
+      },
+      // set the color scale for the choropleth
+      scale: mortgageColorScale,
+      steps: 10,
+      mode: "q",
+      style: function (feature) {
+        //determine value of rentValue and handling if there is not one
+        //use if/else statement
+        let mortgageValue;
+        if (feature.properties.mortgage) {
+          mortgageValue = feature.properties.mortgage[year];
+        } else {
+          mortgageValue = null;
+        }
+        if (mortgageValue === null) {
+          return {
+            color: "white",
+            weight: 1,
+            fillOpacity: 0,
+            fillColor: "gray",
+          };
+        } else {
+          return {
+            color: "white",
+            weight: 0.5,
+            fillOpacity: 0.8,
+          };
+        }
+      },
+      //function to bind pop up and handle state with no data
+      onEachFeature: function (feature, layer) {
+        let mortgageText;
+        if (feature.properties.mortgage) {
+          mortgageText = feature.properties.mortgage[year];
+        } else {
+          mortgageText = `No data`;
+        }
+        layer.bindPopup(`<b>${feature.properties.name}:</b> ${mortgageText}`);
+      },
+    });
+  });
+  L.control.layers(null, mortgageLayers, { collapsed: false }).addTo(myMap2);
+  // have rent 2020 layer open upon loading
+  mortgageLayers[`Mortgage 2020`].addTo(myMap2);
+
+  // create the legend
+  let mortgageLegend = L.control({ position: "bottomleft" });
+  // Add the legend to the map
+  mortgageLegend.onAdd = function () {
+    let div = L.DomUtil.create("div", "info legend");
+    let limits = [1100, 1650, 2200, 2750, 3300, 3850, 4400, 4950, 5500, 6050];
+    let colors = mortgageLayers[`Mortgage 2020`].options.colors;
+    let labels = [];
+
+    // Add min & max values to the legend
+    div.innerHTML =
+      '<div class="labels"><div class="min">' +
+      limits[0] +
+      '</div> \
+          <div class="max">' +
+      limits[limits.length - 1] +
+      "</div></div>";
+
+    // Loop through the limits and add color boxes for each range
+    limits.forEach(function (limit, index) {
+      labels.push('<li style="background-color: ' + colors[index] + '"></li>');
+    });
+
+    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+    return div;
+  };
+
+  // Add the legend to the map
+  mortgageLegend.addTo(myMap2);
+});
 
 // build the bar chart
 function barChart(state) {
